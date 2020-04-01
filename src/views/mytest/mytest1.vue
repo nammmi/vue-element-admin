5bfd18cd2c03237740e881066e7a64f37d07e48b
sxli5 <sxli5@iflytek.com>
2020-04-01 20:27:14 +0800

    beauty

--- src/views/mytest/mytest1.vue
+++ src/views/mytest/mytest1.vue
@@ -1,7 +1,15 @@
 <template>
-  <div class="app-container">hello
+  <div class="app-container">
+    hello
     <div>test</div>
-    <el-table v-loading="listLoading" :data="list" border fit highlight-current-row style="width: 100%">
+    <el-table
+      v-loading="listLoading"
+      :data="list"
+      border
+      fit
+      highlight-current-row
+      style="width: 100%"
+    >
       <el-table-column align="center" label="ID" width="80">
         <template slot-scope="scope">
           <span>{{ scope.row.id }}</span>
@@ -22,15 +30,18 @@
 
       <el-table-column width="100px" label="Importance">
         <template slot-scope="scope">
-          <svg-icon v-for="n in +scope.row.importance" :key="n" icon-class="star" class="meta-item__icon" />
+          <svg-icon
+            v-for="n in +scope.row.importance"
+            :key="n"
+            icon-class="star"
+            class="meta-item__icon"
+          />
         </template>
       </el-table-column>
 
       <el-table-column class-name="status-col" label="Status" width="110">
         <template slot-scope="{row}">
-          <el-tag :type="row.status | statusFilter">
-            {{ row.status }}
-          </el-tag>
+          <el-tag :type="row.status | statusFilter">{{ row.status }}</el-tag>
         </template>
       </el-table-column>
 
@@ -44,57 +55,57 @@
       <el-table-column align="center" label="Actions" width="120">
         <template slot-scope="scope">
           <router-link :to="'/example/edit/'+scope.row.id">
-            <el-button type="primary" size="small" icon="el-icon-edit">
-              Edit
-            </el-button>
+            <el-button type="primary" size="small" icon="el-icon-edit">Edit</el-button>
           </router-link>
         </template>
       </el-table-column>
     </el-table>
-    <pagination v-show="total>0" :total="total" :page.sync="listQuery.page" :limit.sync="listQuery.limit" @pagination="getList" />
-  <!-- <el-button @click="visible = true">按钮</el-button>
+    <pagination
+      v-show="total>0"
+      :total="total"
+      :page.sync="listQuery.page"
+      :limit.sync="listQuery.limit"
+      @pagination="getList"
+    />
+    <!-- <el-button @click="visible = true">按钮</el-button>
     <el-dialog :visible.sync="visible" title="Hello world">
       <p>欢迎使用 Element</p>
-    </el-dialog> -->
-  <el-table
+    </el-dialog>-->
+    <el-table
       v-loading="listLoading2"
       :data="tableData"
       fit
       highlight-current-row
-      style="width: 100%">
-      <el-table-column
-        prop="id"
-        label="id"
-        width="180">
+      style="width: 100%"
+    >
+      <el-table-column prop="id" label="id" width="180">
         <template slot-scope="scope">
           <span>{{ scope.row.id2 }}</span>
         </template>
       </el-table-column>
-      <el-table-column
-        prop="date"
-        label="日期"
-        width="180">
+      <el-table-column prop="date" label="日期" width="180">
         <template slot-scope="scope">
-        <span>{{ scope.row.timestamp2 | parseTime('{y}-{m}-{d} {h}:{i}') }}</span>
+          <span>{{ scope.row.timestamp2 | parseTime('{y}-{m}-{d} {h}:{i}') }}</span>
         </template>
       </el-table-column>
-      <el-table-column
-        prop="name"
-        label="姓名"
-        width="180">
+      <el-table-column prop="name" label="姓名" width="180">
         <template slot-scope="scope">
-        <span>{{ scope.row.name }}</span>
+          <span>{{ scope.row.name }}</span>
         </template>
       </el-table-column>
-      <el-table-column
-        prop="address"
-        label="地址">
+      <el-table-column prop="address" label="地址">
         <template slot-scope="scope">
-        <span>{{ scope.row.addr }}</span>
+          <span>{{ scope.row.addr }}</span>
         </template>
       </el-table-column>
     </el-table>
-    <pagination2 v-show="total>0" :total="total2" :page.sync="listQuery2.page" :limit.sync="listQuery2.limit" @pagination="getDataList" />
+    <pagination2
+      v-show="total>0"
+      :total="total2"
+      :page.sync="listQuery2.page"
+      :limit.sync="listQuery2.limit"
+      @pagination="getDataList"
+    />
   </div>
   <!-- </div> -->
 </template>
